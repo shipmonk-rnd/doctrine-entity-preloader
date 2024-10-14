4d739a4600e9b23d5e157caef6bfc5a74b143ea8
Jan Tvrdík <git@jantvrdik.com>
2024-10-14 09:51:26 +0200

    fix tests compatibility with Doctrine 3.3.0 which reenabled partial (#11)

--- tests/EntityPreloadBlogOneHasManyDeepTest.php
+++ tests/EntityPreloadBlogOneHasManyDeepTest.php
@@ -5,6 +5,8 @@ namespace ShipMonkTests\DoctrineEntityPreloader;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use ShipMonkTests\DoctrineEntityPreloader\Fixtures\Blog\Category;
 use ShipMonkTests\DoctrineEntityPreloader\Lib\TestCase;
+use function array_map;
+use function array_merge;
 
 class EntityPreloadBlogOneHasManyDeepTest extends TestCase
 {
@@ -28,6 +30,46 @@ class EntityPreloadBlogOneHasManyDeepTest extends TestCase
         ]);
     }
 
+    public function testOneHasManyDeepWithWithManualPreloadUsingPartial(): void
+    {
+        $this->skipIfPartialEntitiesAreNotSupported();
+        $this->createCategoryTree(depth: 5, branchingFactor: 5);
+
+        $rootCategories = $this->getEntityManager()->createQueryBuilder()
+            ->select('category')
+            ->from(Category::class, 'category')
+            ->where('category.parent IS NULL')
+            ->getQuery()
+            ->getResult();
+
+        $this->getEntityManager()->createQueryBuilder()
+            ->select('PARTIAL category.{id}', 'subCategory')
+            ->from(Category::class, 'category')
+            ->leftJoin('category.children', 'subCategory')
+            ->where('category IN (:categories)')
+            ->setParameter('categories', $rootCategories)
+            ->getQuery()
+            ->getResult();
+
+        $subCategories = array_merge(...array_map(static fn(Category $category) => $category->getChildren()->toArray(), $rootCategories));
+        $this->getEntityManager()->createQueryBuilder()
+            ->select('PARTIAL subCategory.{id}', 'subSubCategory')
+            ->from(Category::class, 'subCategory')
+            ->leftJoin('subCategory.children', 'subSubCategory')
+            ->where('subCategory IN (:subCategories)')
+            ->setParameter('subCategories', $subCategories)
+            ->getQuery()
+            ->getResult();
+
+        $this->readSubSubCategoriesNames($rootCategories);
+
+        self::assertAggregatedQueries([
+            ['count' => 1, 'query' => 'SELECT * FROM category c0_ WHERE c0_.parent_id IS NULL'],
+            ['count' => 1, 'query' => 'SELECT * FROM category c0_ LEFT JOIN category c1_ ON c0_.id = c1_.parent_id WHERE c0_.id IN (?, ?, ?, ?, ?)'],
+            ['count' => 1, 'query' => 'SELECT * FROM category c0_ LEFT JOIN category c1_ ON c0_.id = c1_.parent_id WHERE c0_.id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'],
+        ]);
+    }
+
     public function testOneHasManyDeepWithFetchJoin(): void
     {
         $this->createCategoryTree(depth: 5, branchingFactor: 5);
@@ -37,13 +79,14 @@ class EntityPreloadBlogOneHasManyDeepTest extends TestCase
             ->from(Category::class, 'category')
             ->leftJoin('category.children', 'subCategories')
             ->leftJoin('subCategories.children', 'subSubCategories')
+            ->where('category.parent IS NULL')
             ->getQuery()
             ->getResult();
 
         $this->readSubSubCategoriesNames($rootCategories);
 
         self::assertAggregatedQueries([
-            ['count' => 1, 'query' => 'SELECT * FROM category c0_ LEFT JOIN category c1_ ON c0_.id = c1_.parent_id LEFT JOIN category c2_ ON c1_.id = c2_.parent_id'],
+            ['count' => 1, 'query' => 'SELECT * FROM category c0_ LEFT JOIN category c1_ ON c0_.id = c1_.parent_id LEFT JOIN category c2_ ON c1_.id = c2_.parent_id WHERE c0_.parent_id IS NULL'],
         ]);
     }
 
